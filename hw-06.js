fce8ea689d8b755352dd073a4e267f311d442cc1
Adelaide <bsharah.a@northeastern.edu>
2023-02-28 21:49:13 -0500

    push 5

--- hw-06.js
+++ hw-06.js
@@ -1,5 +1,6 @@
 console.log("linked");
 
+// frame size
 const FRAME_HEIGHT = 500;
 const FRAME_WIDTH = 500; 
 const MARGINS = {left: 50, right: 50, top: 50, bottom: 50};
@@ -7,24 +8,28 @@ const MARGINS = {left: 50, right: 50, top: 50, bottom: 50};
 const VIS_HEIGHT = FRAME_HEIGHT - MARGINS.top - MARGINS.bottom;
 const VIS_WIDTH = FRAME_WIDTH - MARGINS.left -MARGINS.right;
 
+// left column scatter plot
 const FRAME1 = d3.select("#scatter-1") 
                   .append("svg") 
                     .attr("height", FRAME_HEIGHT)   
                     .attr("width", FRAME_WIDTH)
                     .attr("class", "frame"); 
 
+// middle column scatter plot 
 const FRAME2 = d3.select("#scatter-2")
 				  .append("svg")
 				  	.attr("height", FRAME_HEIGHT)
 				  	.attr("width", FRAME_WIDTH)
 				  	.attr("class", "frame");
 
+// right column bar chart
 const FRAME3 = d3.select("#bar")
 				  .append("svg")
 				  	.attr("height", FRAME_HEIGHT)
 				  	.attr("width", FRAME_WIDTH)
 				  	.attr("class", "frame");
 
+// function to assign each species of flower a color
 function colors(type) {
     if (type == "setosa") {
         return "hotpink"
@@ -38,6 +43,7 @@ function colors(type) {
 }
 
 
+// function to create the first scatter plot of sepal and petal length
  function plot_scatter_1() {
 
   d3.csv("data/iris.csv").then((data) => {
@@ -77,8 +83,10 @@ function colors(type) {
 	});
 }
 
+// plots the first scatter plot
 plot_scatter_1();
 
+// function to create the second scatter plot of sepal and petal width
 function plot_scatter_2() {
 
   d3.csv("data/iris.csv").then((data) => {
@@ -103,23 +111,26 @@ function plot_scatter_2() {
           .attr("opacity", "50%")
           .attr("fill", function(d){return colors(d.Species) });
 
-
+   // makes the x axis 
    FRAME2.append("g")
             .attr("transform", "translate(" + MARGINS.left + "," + (VIS_HEIGHT + MARGINS.top) + ")")
             .call(d3.axisBottom(X_SCALE6).ticks(10))
             .attr("font-size", "15px");
 
-    // make y axis
+    // makes the y axis
     FRAME2.append("g")
             .attr("transform", "translate(" + MARGINS.left + "," + MARGINS.top + ")")
             .call(d3.axisLeft(Y_SCALE6).ticks(10))
             .attr("font-size", "15px");
 
-	FRAME2.call(d3.brush()
+// calls the second scatter plot for brushing
+FRAME2.call(d3.brush()
              .extent([[0,0], [FRAME_WIDTH, FRAME_HEIGHT]])
              .on("start brush", update))
              .on("end", () => { });
 
+// function for the brush and link event so scatter plot 1 and the bar chart
+// are linked to brushed over area in scatter plot 2
 function update(event) {
 	let extent = event.selection;
 	points_1.classed("selected", function(d){ return isBrushed(extent, X_SCALE6(d.Sepal_Width) + MARGINS.left, Y_SCALE6(d.Petal_Width) + MARGINS.top)} )
@@ -127,6 +138,7 @@ function update(event) {
 	bar.classed("selected", function(d){ return isBrushed(extent, X_SCALE6(d.Sepal_Width) + MARGINS.left, Y_SCALE6(d.Petal_Width) + MARGINS.top)} )
 }
 
+// function for the coordinates when a point is brushed over
 function isBrushed(brush_coords, cx, cy) {
        var x0 = brush_coords[0][0],
            x1 = brush_coords[1][0],
@@ -138,8 +150,10 @@ function isBrushed(brush_coords, cx, cy) {
 	});
 }
 
+// plots the second scatter plot 
 plot_scatter_2();
 
+// function to create a bar chart of the species 
 function plot_bar() {
 
   d3.csv("data/iris.csv").then((data) => {
@@ -162,13 +176,13 @@ function plot_bar() {
 			  .attr("height", (d) => {return VIS_HEIGHT - Y_SCALE6(50)})
 			  .style("fill", function(d){return colors(d.Species) });
 
-
+	// makes the x axis
    FRAME3.append("g")
         .attr("transform", "translate(" + MARGINS.left + "," + (VIS_HEIGHT + MARGINS.top) + ")")
         .call(d3.axisBottom(X_SCALE6).ticks(10))
         .attr("font-size", "15px");
 
-
+    // makes the y axis
     FRAME3.append("g")
         .attr("transform", "translate(" + MARGINS.left + "," + MARGINS.top + ")")
         .call(d3.axisLeft(Y_SCALE6).ticks(10))
@@ -182,6 +196,7 @@ function plot_bar() {
 	});
 }
 
+// plots the bar graph
 plot_bar();
 
 
